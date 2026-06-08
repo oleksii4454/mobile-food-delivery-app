c52bd4ffdd85fa732a73275eb8881337d5b76bf4
oleksii4454 <10alex0520@gmail.com>
2026-06-08 10:48:11 +0300

    update: add orders routes to express app

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
+require('./src/config/db');
+
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./src/modules/auth/auth.routes');
+const ordersRoutes = require('./src/modules/orders/orders.routes');
 
 const app = express();
 
@@ -9,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Сервер запущено на порту ${PORT}`));
