03bed63df054511cbec9f880d6dca171fdc90a6c
oleksii4454 <10alex0520@gmail.com>
2026-06-11 05:00:19 +0300

    build: add prisma schema and config

---
+++ backend/prisma.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from '@prisma/config';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
